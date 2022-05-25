046981dd679dc07ab4dc6425d7f670afc46e62a4
Jonathan <kim.jonathan426@gmail.com>
2022-05-25 17:22:56 -0400

    Phase 2 Complete

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const { restart } = require("nodemon");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
+
+app.use(cookieParser())
+app.use(express.urlencoded());
+const csrfProtection = csrf({ cookie: true });
+
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -11,7 +19,9 @@ app.get("/", (req, res) => {
   });
 });
 
-
+app.get("/create", csrfProtection, (req, res) => {
+  res.render("create-normal-user", { users, title: "User Form", csrfToken: req.csrfToken() })
+})
 
 const users = [
   {
